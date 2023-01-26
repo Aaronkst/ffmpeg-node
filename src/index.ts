9dc7b45817495676d7f7358fc20f3b30b2cf93ba
Aaron Chen <aaron.kst99@gmail.com>
2023-01-26 15:00:29 +0630

    socket.io options

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,8 @@ const browserToRtmpServer = new BrowserToRtmpServer(server, {
     cors: {
       origin: ["http://localhost:3000"],
       methods: ["GET", "POST"],
+      credentials: true,
     },
-    transports: ["websocket", "polling"],
-    allowEIO3: true,
   },
   hooks: {
     start: (socket, config) => {
