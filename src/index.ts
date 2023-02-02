820100e8e739c4727445737187f406110c31f746
Aaron Chen <aaron.kst99@gmail.com>
2023-02-02 11:48:17 +0630

    IVS

--- src/index.ts
+++ src/index.ts
@@ -26,18 +26,18 @@ const browserToRtmpServer = new BrowserToRtmpServer(server, {
     path: "/",
     allowEIO3: true,
   },
-  hooks: {
-    start: (socket, config) => {
-      // for instance, you can here access the socket associated to the current request:
-      // const token = socket.handshake.auth.token; // retrieve the auth token
-      // ...
-      const rtmp = "rtmp://18.180.45.42:1935/proto-1/live-1"; // you can generate here the RTMP endpoint url according to your need:
-      return {
-        ...config,
-        rtmp,
-      };
-    },
-  },
+  // hooks: {
+  //   start: (socket, config) => {
+  //     // for instance, you can here access the socket associated to the current request:
+  //     // const token = socket.handshake.auth.token; // retrieve the auth token
+  //     // ...
+  //     const rtmp = "rtmp://18.180.45.42:1935/proto-1/live-1"; // you can generate here the RTMP endpoint url according to your need:
+  //     return {
+  //       ...config,
+  //       rtmp,
+  //     };
+  //   },
+  // },
 });
 browserToRtmpServer.on("error", (uuid, error) => {
   console.log(`Error for connection ${uuid}: ${error.message}`);
